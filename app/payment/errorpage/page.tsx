997c33029b7b93f4297d6feb5e5c8cff9b8e9885
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-07-11 08:08:43 +0100

    Errorpage UI

--- app/payment/errorpage/page.tsx
+++ app/payment/errorpage/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { Button } from '@/components/ui/button';
 
 const ErrorPage = () => {
   return (
@@ -18,6 +19,9 @@ const ErrorPage = () => {
           Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.
         </p>
       </div>
+      <Button className="w-auto bg-[#BAEF23] hover:bg-lime-500 rounded-full">
+      <a href="/" className="block p-4 font-bold text-[#020721]">Go Home</a>
+    </Button>
     </div>
   );
 };
