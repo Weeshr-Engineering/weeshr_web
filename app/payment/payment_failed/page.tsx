e49be54d4319ec5ac68f720ed4aebba8295cd5ed
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-07-11 08:01:15 +0100

    success and failed payment ui

---
+++ app/payment/payment_failed/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { Button } from '@/components/ui/button'
+
+const ErrorPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-[#F8F9FF] p-4">
+      <img
+        className="mb-4"
+        src="https://res.cloudinary.com/dufimctfc/image/upload/v1720680326/payment_failed_ro0qx3.svg"
+        alt="gradient"
+      />
+      <div className="flex flex-col items-center justify-center mb-5 text-center">
+        <p className="text-2xl font-bold text-[#111827] md:text-3xl lg:text-4xl">
+          Payment Failed          
+        </p>
+        <p className="mt-4 text-sm text-[#6B7280] md:text-lg lg:text-xl">
+        There was an issue with your payment. Please try again
+        </p>
+      </div>
+      <Button className="w-auto bg-[#BAEF23] hover:bg-lime-500 rounded-full">
+      <a href="/" className="block p-4 font-bold text-[#020721]">Go Home</a>
+    </Button>
+    </div>
+  );
+};
+
+export default ErrorPage;
