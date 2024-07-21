76b2b85e22ad64acdea9fe3530c6a97bfb0270bc
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-07-21 11:04:53 +0100

    corrected the errorpage

--- app/payment/errorpage/page.tsx
+++ app/payment/errorpage/page.tsx
@@ -2,14 +2,17 @@
 
 import React from "react";
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-[#F8F9FF] p-4">
-      <img
+      <Image
         className="mb-4"
+        width={397}
+        height={90}
         src="https://res.cloudinary.com/dufimctfc/image/upload/v1719761079/errorpage_kkjhkb.svg"
-        alt="gradient"
+        alt="Error Image"
       />
       <div className="flex flex-col items-center justify-center mb-5 text-center">
         <p className="text-2xl font-bold text-[#111827] md:text-3xl lg:text-4xl">
@@ -20,8 +23,8 @@ const ErrorPage = () => {
         </p>
       </div>
       <Button className="w-auto bg-[#BAEF23] hover:bg-lime-500 rounded-full">
-      <a href="/" className="block p-4 font-bold text-[#020721]">Go Home</a>
-    </Button>
+        <a href="/" className="block p-4 font-bold text-[#020721]">Go Home</a>
+      </Button>
     </div>
   );
 };
