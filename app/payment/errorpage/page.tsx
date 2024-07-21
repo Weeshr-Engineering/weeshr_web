a6b7820fd725f307b182428085f305828d2d1ceb
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-07-21 14:26:05 +0100

    corrected the Ui

--- app/payment/errorpage/page.tsx
+++ app/payment/errorpage/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const ErrorPage = () => {
   return (
@@ -22,9 +23,12 @@ const ErrorPage = () => {
           Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.
         </p>
       </div>
-      <Button className="w-auto bg-[#BAEF23] hover:bg-lime-500 rounded-full">
-        <a href="/" className="block p-4 font-bold text-[#020721]">Go Home</a>
-      </Button>
+      
+      <Link href="/" passHref>
+        <Button className="w-auto bg-[#BAEF23] hover:bg-lime-500 rounded-full  font-bold text-[#020721]">
+          Go Home
+        </Button>
+      </Link>
     </div>
   );
 };
