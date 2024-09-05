890331e21628de77b4d4b10c564d7c0f07468243
Opeyemi Eugene Kolapo <kolapo.eugene@gmail.com>
2024-09-05 23:59:30 +0100

    dn

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/weeshr_web/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 // Define the type for social media links
 interface SocialMediaLink {
