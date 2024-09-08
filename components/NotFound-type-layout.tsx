32710cdc15a28a748547da42299dc82f5d23250f
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-09-08 19:42:13 +0100

    fix 2

--- components/NotFound-type-layout.tsx
+++ components/NotFound-type-layout.tsx
@@ -16,7 +16,7 @@ interface ErrTypeLayoutProps {
 export const ErrTypeLayout: React.FC<ErrTypeLayoutProps> = ({ children }) => {
   return (
     <InterfaceLayout>
-      <div className="w-4/5 mx-auto flex items-center justify-center  h-44">
+      <div className="w-4/5 mx-auto flex items-center justify-center  min-h-100vh">
         <Header />
       </div>
       <WidthLayout narrow={true}>
